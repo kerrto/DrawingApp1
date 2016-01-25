c154d686d557a899cdb5f8675296505cbc5992c0
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-24 20:07:08 -0500

    Initial Commit

---
+++ DrawingApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingApp1
+//
+//  Created by Kerry Toonen on 2016-01-24.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingApp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingApp1
+//
+//  Created by Kerry Toonen on 2016-01-24.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
